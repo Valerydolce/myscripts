6656fa7696778abbb542a7e57bcfc260788cf613
Paul <valerydolce@gmail.com>
2022-03-01 23:55:58 -0500

    added a linux script which will install packages on centos

---
+++ pkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+    #Author : yvalcorp
+        #Date : Mars 1st, 2022
+
+	## ---------- script that Install some packages in Linux -----------------
+
+#Install finger - it is a user information lookup tool which gives information about users logged in
+yum install finger -y 
+# Install Curl - Used to transfer data from or to a server
+yum install curl -y 
+# Install ZIP - Utility to zip and unzip files
+yum install zip -y 
+# Install VIM - File editor
+yum install vim -y 
+
