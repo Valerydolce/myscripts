27f5d2c9b680935bf2e3704e63df6a632ea2215c
Paul <valerydolce@gmail.com>
2022-03-02 02:03:55 -0500

    added the env_variable_check.sh file

---
+++ env_variable_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check the current user's environment variable options
+#!/bin/bash
+	# Description: Check current user's environment variables
+	# Author: yvalcorp
+	# Date: 03 Mars 2022
+echo “Hello there, your username is ${USER}”
+echo”Your User ID is ${UID}”
+echo ”Your Shell is ${SHELL} ”
+echo “Your home directory is ${HOME}”
+echo “This server’s Hostname is  ${HOSTNAME}”
+
